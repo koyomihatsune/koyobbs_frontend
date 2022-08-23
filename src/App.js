7c77232e5f7c67660b934dd7e32abc26a3ae4bd7
Nguyen Viet Anh <anh_nv@flinters.vn>
2022-08-23 16:05:11 +0700

    added Fluent UI React

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import { Button, Checkbox } from '@fluentui/react-components';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button>Example</Button>
+      <Checkbox label="Checkbox" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
+
+import App from './App';
+
+ReactDOM.render(
+  <FluentProvider theme={teamsLightTheme}>
+    <App />
+  </FluentProvider>,
+  document.getElementById('root'),
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

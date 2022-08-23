cf78831425bd5313da980ca01061152229611ae4
Nguyen Viet Anh <anh_nv@flinters.vn>
2022-08-23 18:12:21 +0700

    made some minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import logo from './logo.svg';
 import { Button, Checkbox } from '@fluentui/react-components';
 
-
 function App() {
   return (
     <div className="App">
-      <Button>Example</Button>
-      <Checkbox label="Checkbox" />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
-
+// import './index.css'
 import App from './App';
 
 ReactDOM.render(
   <FluentProvider theme={teamsLightTheme}>
-    <App />
+    <App>
+    </App>
   </FluentProvider>,
   document.getElementById('root'),
 );
